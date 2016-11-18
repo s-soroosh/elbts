e6fb4cb70e6cd07eff76b2643dc0ad312a945eb4
Soroosh Sarabadani <soroosh.sarabadani@zalando.de>
2016-11-18 11:34:05 +0100

    overall_response_time metric has been added

--- src/main/scala/de/zalando/elbts/actors/KairosMetricsBuilder.scala
+++ src/main/scala/de/zalando/elbts/actors/KairosMetricsBuilder.scala
@@ -1,6 +1,7 @@
 package de.zalando.elbts.actors
 
 import akka.actor.{Actor, ActorRef}
+import de.zalando.elbts.Tagger
 import de.zalando.elbts.messages.LogItem
 import org.kairosdb.client.builder.MetricBuilder
 
@@ -9,19 +10,18 @@ import scala.collection.JavaConverters._
 /**
   * @author ssarabadani <soroosh.sarabadani@zalando.de>
   */
-class KairosMetricsBuilder(target: ActorRef) extends Actor {
+class KairosMetricsBuilder(target: ActorRef,tagger:Tagger) extends Actor {
   override def receive: Receive = {
     case logItem: LogItem => {
-
-      val tags = Map("elb" -> logItem.elb, "endpoint" -> logItem.request.path).asJava
+      val tags= tagger.tag(logItem).asJava
 
       val builder = MetricBuilder.getInstance()
-      builder.addMetric("backend_response_time").addDataPoint(logItem.issueDate.getMillis).addTags(tags)
-      builder.addMetric("backend_status_code").addDataPoint(logItem.backendStatusCode).addTags(tags)
-      builder.addMetric("elb_status_code").addDataPoint(logItem.backendStatusCode).addTags(tags)
+      builder.addMetric("backend_response_time").addDataPoint(logItem.issueDate.getMillis, logItem.backendProcessingTime).addTags(tags)
+      builder.addMetric("overall_response_time").addDataPoint(logItem.issueDate.getMillis, logItem.backendProcessingTime + logItem.responseProcessingTime + logItem.requestProcessingTime).addTags(tags)
+      builder.addMetric("backend_status_code").addDataPoint(logItem.issueDate.getMillis, logItem.backendStatusCode).addTags(tags)
+      builder.addMetric("elb_status_code").addDataPoint(logItem.issueDate.getMillis, logItem.backendStatusCode).addTags(tags)
 
       target ! builder
-
     }
   }
 }
