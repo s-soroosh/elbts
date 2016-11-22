960a3d3fc87f8e8a6aa2c71067f1e5e6080f4689
Soroosh Sarabadani <soroosh.sarabadani@zalando.de>
2016-11-22 10:22:23 +0100

    RestAPI bound

--- src/main/scala/de/zalando/elbts/Application.scala
+++ src/main/scala/de/zalando/elbts/Application.scala
@@ -1,6 +1,10 @@
 package de.zalando.elbts
 
-import akka.actor.{ActorRef, ActorSystem, Props}
+import akka.actor.ActorSystem
+import akka.event.Logging
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.server.Route
+import akka.stream.ActorMaterializer
 import akka.util.Timeout
 import com.typesafe.config.{Config, ConfigFactory}
 import de.zalando.elbts.actors._
@@ -15,11 +19,27 @@ object Application extends App with RequestTimeout with AkkaInjectable {
 
   val config = ConfigFactory.load()
 
+  private val host: String = config.getString("http.host")
+  private val port: Int = config.getInt("http.port")
   implicit val module = new ActorModule :: new AkkaModule
   implicit val system = inject[ActorSystem]
+  implicit val ec = system.dispatcher
   val queueActor = injectActorRef[QueueReader]("queue-reader")
+  val log = Logging(system, "elbts")
 
 
+  private val route: Route = new RestRoutes().healthcheckRoute
+  implicit val actorMaterializer = ActorMaterializer()
+
+  val bindingFuture = Http().bindAndHandle(route, host, port)
+
+
+  bindingFuture.map { binding =>
+    log.info(s"API bound  to ${binding.localAddress}")
+  }.onFailure {
+    case ex: Exception =>
+      log.error(ex, "Failed to bind to {}:{}", host, port)
+  }
   queueActor ! Run()
 
 }
